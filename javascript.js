ce188db687991c1cacc305b7471ee38eb426abdf
hanzalla <hanzialam034@gmail.com>
2021-06-09 11:49:54 -0700

    progress bar edited

--- javascript.js
+++ javascript.js
@@ -9,20 +9,27 @@ var next2 = document.getElementById("next3");
 var back1 = document.getElementById("back1");
 var back2 = document.getElementById("back2");
 
+var progress = document.getElementById('progress');
+
 next1.onclick = function () {
+
     form1.style.left = "450px";
     form2.style.left = "40px";
+    progress.style.width = "240px";
 }
 back1.onclick = function () {
     form1.style.left = "40px";
     form2.style.left = "450px";
+    progress.style.width = "100px";
 }
 next2.onclick = function () {
     form2.style.left = "450px";
     form3.style.left = "40px";
+    progress.style.width = "360px";
 }
 back2.onclick = function () {
     form2.style.left = "40px";
     form3.style.left = "450px";
+    progress.style.width = "240px";
 }
 
