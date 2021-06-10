303b6687ca64e177e5f9e034e95da8bd16e00764
hanzalla <hanzialam034@gmail.com>
2021-06-10 01:54:25 -0700

    final commit

--- javascript.js
+++ javascript.js
@@ -11,12 +11,30 @@ var back2 = document.getElementById("back2");
 
 var progress = document.getElementById('progress');
 
+const forms = document.getElementById('forms');
+const fnameValue = document.getElementById('fname');
+
+
+forms.addEventListener('next2', (e) => {
+    e.preventDefault;
+
+    checkInput();
+});
+function checkInput(params) {
+    const fnameValue = fname.value.trim();
+}
+
 next1.onclick = function () {
 
     form1.style.left = "450px";
     form2.style.left = "40px";
     progress.style.width = "240px";
+
 }
+
+
+
+
 back1.onclick = function () {
     form1.style.left = "40px";
     form2.style.left = "450px";
